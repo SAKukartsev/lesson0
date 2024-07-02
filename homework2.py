56fd96454439db40889f51b5670138a3f897e244
SAKukartsev <sergcover@yandex.ru>
2024-07-02 22:17:00 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_completed_tasks=12
+Number_of_hours_spent=1.5
+Course_name='Pyton'
+Time_for_one_task=Number_of_hours_spent/number_of_completed_tasks
+print("Курс:",Course_name,", всего задач:",number_of_completed_tasks,", затрачено часов:",Number_of_hours_spent, ", среднее время выполнения ", Time_for_one_task," часа.")
